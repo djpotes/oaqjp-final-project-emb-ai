9ccfa02b4131be81b991301823a4d2c8aabf2fe4
Darwin Potes <darwinpotes2102@live.com>
2026-02-26 17:26:33 -0500

    add package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    inputjson = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json=inputjson, headers=header)
+    responseJson = json.loads(response.text)
+    emotions = responseJson["emotionPredictions"][0]["emotion"]
+    response_dominant_emotion = {
+        "anger": emotions["anger"], 
+        "disgust": emotions["disgust"], 
+        "fear": emotions["fear"], 
+        "joy": emotions["joy"], 
+        "sadness": emotions["sadness"]
+    }
+    highest_score_key = max(emotions, key=emotions.get)
+    response_dominant_emotion["dominant_emotion"] = highest_score_key
+    return response_dominant_emotion
+
+  
