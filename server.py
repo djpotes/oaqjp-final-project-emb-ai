2a3afd704ede367a42a75b9f23c9475a626acb50
Darwin Potes <darwinpotes2102@live.com>
2026-02-26 21:00:03 -0500

    addcode

--- server.py
+++ server.py
@@ -8,7 +8,9 @@ def emotionDetector():
     if emotion["dominant_emotion"] == "None":
         return "Invalid text! Please try again!" 
     else:
-        return "For the given statement, the system response is 'anger': "+emotion["anger"]+", 'disgust': "+emotion["disgust"]+", 'fear': "+emotion["fear"]+", 'joy': "+emotion["joy"]+" and 'sadness': "+emotion["sadness"]+". The dominant emotion is "+emotion["dominant_emotion"]
+        return "For the given statement, the system response is 'anger': "+emotion["anger"]+
+        ", 'disgust': "+emotion["disgust"]+", 'fear': "+emotion["fear"]+", 'joy': 
+        "+emotion["joy"]+" and 'sadness': "+emotion["sadness"]+". The dominant emotion is "+emotion["dominant_emotion"]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
