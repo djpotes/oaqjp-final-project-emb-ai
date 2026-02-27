be81122ded5fa9857485c8a5bcd0c139a169723d
Darwin Potes <darwinpotes2102@live.com>
2026-02-26 21:49:20 -0500

    Add

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
+"""Expose emotion server"""
 from flask import Flask, request
 from EmotionDetection import emotion_detection
 app = Flask(__name__)
 @app.route('/emotionDetector')
-def emotionDetector():
-    textToAnalyze = request.args.get('textToAnalyze')
-    emotion = emotion_detection.emotion_detector(textToAnalyze)
+def detector():
+    """To get emotion"""
+    text = request.args.get('textToAnalyze')
+    emotion = emotion_detection.emotion_detector(text)
     if emotion["dominant_emotion"] == "None":
-        return "Invalid text! Please try again!" 
-    else:
-        return "For the given statement, the system response is 'anger': "+str(emotion["anger"])+", 'disgust': "+str(emotion["disgust"])+", 'fear': "+str(emotion["fear"])+", 'joy': "+str(emotion["joy"])+" and 'sadness': "+str(emotion["sadness"])+". The dominant emotion is "+str(emotion["dominant_emotion"])
+        return "Invalid text! Please try again!"
+    return "For the given statement, the system response is 'anger': "+str(emotion["anger"])+", 'disgust': "+str(emotion["disgust"])+", 'fear': "+str(emotion["fear"])+", 'joy': "+str(emotion["joy"])+" and 'sadness': "+str(emotion["sadness"])+". The dominant emotion is "+str(emotion["dominant_emotion"])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
